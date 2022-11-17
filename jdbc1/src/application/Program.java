c29061b1b935aaaec8a438719645f44aea793062
Wanderson <wandersongamer2@gmail.com>
2022-11-17 11:08:31 -0300

    logIn e signIn implementados

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -1,38 +1,54 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
-import db.DB;
+import db.connections.Connections;
 
 
 public class Program {
 
+
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		try {
-			conn = DB.getConnection();
+		Scanner sc = new Scanner(System.in);
+		Connections conn = new Connections();
+		
+		System.out.println("Sign In | Sign Up");
+		int n = 1;
+		while (n != 1 || n != 2) {
+			n = sc.nextInt();
 			
-			st = conn.createStatement();
-			
-			rs = st.executeQuery("SELECT * FROM javaBank");
-			
-			while (rs.next()) {
-				System.out.println(rs.getInt("client_id")+", " + rs.getString("client_name"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeResultSet(rs);
-			DB.closeConnection();
-		}
-	}
+			if (n == 1) {
+				sc.nextLine();
+				System.out.println("----Log In----");
 
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				
+				
+				System.out.print("Password: ");
+				String pass = sc.nextLine();
+				
+				String result = conn.logIn(name, pass);
+				
+				System.out.println(result);
+			}
+			else if (n == 2) {
+				sc.nextLine();
+				System.out.println("----Create User----");
+
+				System.out.print("Name: ");
+				String name = sc.nextLine();
+				
+				
+				System.out.print("Password: ");
+				String pass = sc.nextLine();
+				
+				conn.createUser(name, pass);
+			}
+		}
+		
+		sc.close();
+		
+	}
 }
